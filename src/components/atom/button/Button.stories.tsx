133496a4322e407f5d30410f6561d2eb8c514500
Jerry Wang <jerryappleid761208@gmail.com>
2022-12-03 10:27:07 -0800

    add secondary

--- src/components/atom/button/Button.stories.tsx
+++ src/components/atom/button/Button.stories.tsx
@@ -1,25 +1,20 @@
-// Button.stories.ts|tsx
-
 import React from 'react';
 
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import Button from '.';
+import Button, { ButtonProps } from '.';
 
 export default {
-  /* 👇 The title prop is optional.
-   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
-   * to learn how to generate automatic titles
-   */
   title: 'Atoms/Button',
   component: Button,
 } as ComponentMeta<typeof Button>;
 
-//👇 We create a “template” of how args map to rendering
 const Template: ComponentStory<typeof Button> = (args) => (
   <Button {...args}>Button</Button>
 );
 
-//👇 Each story then reuses that template
 export const Primary = Template.bind({});
-Primary.args = { variant: 'primary', label: 'Button' };
+Primary.args = { variant: 'primary' };
+
+export const Secondary = Template.bind({});
+Secondary.args = { variant: 'secondary' };
